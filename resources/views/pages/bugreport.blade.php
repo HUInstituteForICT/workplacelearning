1b621347e80ce2aa70463f74a6fa20d6cf154547
Max Cassee <max_cassee@hotmail.com>
2016-10-29 00:13:00 +0200

    Issue opgelost waarbij de textarea buiten de container kon overflowen.

--- resources/views/pages/bugreport.blade.php
+++ resources/views/pages/bugreport.blade.php
@@ -20,7 +20,7 @@
                 <p>Heb je een tip hoe we deze site kunnen verbeteren, of heb je iets gevonden wat niet werkt? Laat het ons weten!</p>
                 {!! Form::open(array('id' => 'taskForm', 'class' => 'form-horizontal well', 'url' => URL::to('bugreport/create', array(), true))) !!}
                     <input type="text" name="onderwerp" placeholder="Wat wil je doorgeven?" /><br><br />
-                    <textarea name="uitleg" cols="80" rows="10" placeholder="Bijvoorbeeld: Ik zou graag een andere rapportage terugzien op de analyse pagina. Kan deze worden toegevoegd?"></textarea><br />
+                    <textarea style="max-width: 100%;" name="uitleg" cols="80" rows="10" placeholder="Bijvoorbeeld: Ik zou graag een andere rapportage terugzien op de analyse pagina. Kan deze worden toegevoegd?"></textarea><br />
                     <input type="submit" value="Verstuur" />
                 {!! Form::close() !!}
             </div>
