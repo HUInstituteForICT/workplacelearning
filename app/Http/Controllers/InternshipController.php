eb9fde7335332f29e6afab5b5c2a814852bb863e
Max Cassee <max_cassee@hotmail.com>
2016-12-23 10:18:21 +0100

    Make the internship assignment validation more lenient.

--- app/Http/Controllers/InternshipController.php
+++ app/Http/Controllers/InternshipController.php
@@ -50,7 +50,7 @@ class InternshipController extends Controller{
             'numhours'              => 'required|digits_between:1,5',
             'startdate'             => 'required|date|after:'.date("Y-m-d", strtotime('-6 months')),
             'enddate'               => 'required|date|after:startdate',
-            'internshipAssignment'  => 'required|regex:/^[0-9a-zA-Z ()-,.*&:_+=%$@!]*$/|min:15|max:500',
+            'internshipAssignment'  => 'required|regex:/^[0-9a-zA-Z ()-,.*&:_+=%$@!?;]*$/|min:15|max:500',
             'isActive'              => 'sometimes|required|in:1,0'
         ]);
 
