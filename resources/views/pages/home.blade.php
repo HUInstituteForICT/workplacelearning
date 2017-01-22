f7ad98a84056317b41cb46f2432f1fe8fd288d93
Dylan van Gils <dylanvgils@users.noreply.github.com>
2017-01-22 13:40:12 +0100

    bugfix in home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 @stop
 @section('content')
         <div class="container-fluid">
-            @if(Auth::user()->getCurrentWorkplacePeriod() == NULL)
+            @if(Auth::user()->getCurrentWorkplaceLearningPeriod() == NULL)
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="alert alert-notice">
@@ -34,9 +34,9 @@
 
             <div class="row">
                 <div class="col-lg-12">
-                    @if(Auth::user()->getCurrentWorkplacePeriod() != null && Auth::user()->getCurrentWorkplacePeriod()->hasLoggedHours())
+                    @if(Auth::user()->getCurrentWorkplaceLearningPeriod() != null && Auth::user()->getCurrentWorkplaceLearningPeriod()->hasLoggedHours())
                         <h1>{{ Lang::get('dashboard.lastWZHtitle') }}</h1>
-                        @foreach(Auth::user()->getCurrentWorkplacePeriod()->getLastWerkzaamheden(5) as $wzh)
+                        @foreach(Auth::user()->getCurrentWorkplaceLearningPeriod()->getLastWerkzaamheden(5) as $wzh)
                             <div class="dash-bar">
                                 <?php
                                 $fmt = new IntlDateFormatter(
