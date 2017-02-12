34131407a25dd2cef5baf5555ae12c7a831764b4
Max Cassee <max_cassee@hotmail.com>
2017-02-12 14:39:24 +0100

    Changed head to use CDN references instead of local files

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -19,14 +19,13 @@
         <meta name="_token" content="{!! csrf_token() !!}" />
         <!-- Bootstrap -->
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker3.min.css">
-        <script src="{{ secure_asset('js/transition.js') }}"></script>
-        <script src="{{ secure_asset('js/collapse.js') }}"></script>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.45/css/bootstrap-datetimepicker.min.css">
+
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <!-- Moment Time localization -->
-        <script src="{{ secure_asset('js/moment-with-locales.js') }}"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment-with-locales.min.js"></script>
         <!-- Datetime Picker -->
-        <script type="text/javascript" src="{{ secure_asset('js/bootstrap-datetimepicker.js') }}"></script>
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.45/js/bootstrap-datetimepicker.min.js"></script>
         <!-- Chart.JS library -->
         <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.min.js"></script>
         <!-- Font Awesome libary -->
