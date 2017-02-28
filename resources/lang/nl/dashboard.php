80ea79715945998b536efea9fde09cdae4601987
Max Cassee <max_cassee@hotmail.com>
2017-02-28 16:51:50 +0100

    New dutch validation error translations, should help finding errors in forms easier for users. Move of notifications to new lang/notifications file rather than being page specific.

--- resources/lang/nl/dashboard.php
+++ resources/lang/nl/dashboard.php
@@ -10,7 +10,6 @@ return [
     "body"  => "Welkom op het subdomein van de werkplekleren applicatie van de Hogeschool Utrecht.",
     "lastWZHtitle" => "Laatste Activiteit",
     "hours" => "uur",
-    "nointernshipactive" => "Je hebt momenteel geen actieve stage. Klik <a href=\"%s\">hier</a> om er een toe te voegen."
 
 ];
 
