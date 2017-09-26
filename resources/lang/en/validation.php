e829bff4f21109ced62012c9fd27d89103e859aa
Rogier Knoester <knoesterrogier@gmail.com>
2017-09-26 13:16:53 +0200

    add translations & translations manager

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,170 +1,147 @@
 <?php
 
-return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
-    'accepted'             => 'The :attribute must be accepted.',
-    'active_url'           => 'The :attribute is not a valid URL.',
-    'after'                => 'The :attribute must be a date after :date.',
-    'alpha'                => 'The :attribute may only contain letters.',
-    'alpha_dash'           => 'The :attribute may only contain letters, numbers, and dashes.',
-    'alpha_num'            => 'The :attribute may only contain letters and numbers.',
-    'array'                => 'The :attribute must be an array.',
-    'before'               => 'The :attribute must be a date before :date.',
-    'between'              => [
-        'numeric' => 'The :attribute must be between :min and :max.',
-        'file'    => 'The :attribute must be between :min and :max kilobytes.',
-        'string'  => 'The :attribute must be between :min and :max characters.',
-        'array'   => 'The :attribute must have between :min and :max items.',
-    ],
-    'boolean'              => 'The :attribute field must be true or false.',
-    'confirmed'            => 'The :attribute confirmation does not match.',
-    'date'                 => 'The :attribute is not a valid date.',
-    'date_format'          => 'The :attribute does not match the format :format.',
-    'different'            => 'The :attribute and :other must be different.',
-    'digits'               => 'The :attribute must be :digits digits.',
-    'digits_between'       => 'The :attribute must be between :min and :max digits.',
-    'dimensions'           => 'The :attribute has invalid image dimensions.',
-    'distinct'             => 'The :attribute field has a duplicate value.',
-    'email'                => 'The :attribute must be a valid email address.',
-    'exists'               => 'The selected :attribute is invalid.',
-    'file'                 => 'The :attribute must be a file.',
-    'filled'               => 'The :attribute field is required.',
-    'image'                => 'The :attribute must be an image.',
-    'in'                   => 'The selected :attribute is invalid.',
-    'in_array'             => 'The :attribute field does not exist in :other.',
-    'integer'              => 'The :attribute must be an integer.',
-    'ip'                   => 'The :attribute must be a valid IP address.',
-    'json'                 => 'The :attribute must be a valid JSON string.',
-    'max'                  => [
-        'numeric' => 'The :attribute may not be greater than :max.',
-        'file'    => 'The :attribute may not be greater than :max kilobytes.',
-        'string'  => 'The :attribute may not be greater than :max characters.',
-        'array'   => 'The :attribute may not have more than :max items.',
-    ],
-    'mimes'                => 'The :attribute must be a file of type: :values.',
-    'mimetypes'            => 'The :attribute must be a file of type: :values.',
-    'min'                  => [
-        'numeric' => 'The :attribute must be at least :min.',
-        'file'    => 'The :attribute must be at least :min kilobytes.',
-        'string'  => 'The :attribute must be at least :min characters.',
-        'array'   => 'The :attribute must have at least :min items.',
-    ],
-    'not_in'               => 'The selected :attribute is invalid.',
-    'numeric'              => 'The :attribute must be a number.',
-    'present'              => 'The :attribute field must be present.',
-    'regex'                => 'The :attribute format is invalid.',
-    'required'             => 'The :attribute field is required.',
-    'required_if'          => 'The :attribute field is required when :other is :value.',
-    'required_unless'      => 'The :attribute field is required unless :other is in :values.',
-    'required_with'        => 'The :attribute field is required when :values is present.',
-    'required_with_all'    => 'The :attribute field is required when :values is present.',
-    'required_without'     => 'The :attribute field is required when :values is not present.',
-    'required_without_all' => 'The :attribute field is required when none of :values are present.',
-    'same'                 => 'The :attribute and :other must match.',
-    'size'                 => [
-        'numeric' => 'The :attribute must be :size.',
-        'file'    => 'The :attribute must be :size kilobytes.',
-        'string'  => 'The :attribute must be :size characters.',
-        'array'   => 'The :attribute must contain :size items.',
-    ],
-    'string'               => 'The :attribute must be a string.',
-    'timezone'             => 'The :attribute must be a valid zone.',
-    'unique'               => 'The :attribute has already been taken.',
-    'uploaded'             => 'The :attribute failed to upload.',
-    'url'                  => 'The :attribute format is invalid.',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
-    |
-    */
-
-    'custom' => [
-        'res_person'   => [
-            "required" => 'The "with whom?" field is required.',
-            "exists"   => 'This "with whom?" does not exist.',
-        ],
-        "new_rp"       => [
-            "required" => 'The description for the "with whom?" field is required.',
-            "max"      => 'The description for the "with whom?" field can only be 45 characters long.',
-            "regex"    => 'The description for the "with whom?" field has an invalid format.',
-        ],
-        'res_material'        => [
-            "required" => 'The "with which theory?" field is required.',
-            "exists"   => 'This "with which theory?" does not exist.',
-        ],
-        'new_rm'              => [
-            "required" => 'The description for the "with which theory?" field is required.',
-            "max"      => 'The description for the "with which theory?" field  can only be 45 characters long.',
-            "regex"    => 'The description for the "with which theory?" field  has an invalid format.',
-        ],
-        "competence"          => [
-            "exists" => "The selected :attribute is invalid",
-        ],
-        "learning_goal"       => [
-            "exists" => "The selected :attribute is invalid",
-        ],
-        "res_material_detail" => [
-            "required_unless" => "The :attribute field is required.",
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
-    |
-    */
-
-    'attributes' => [
-        "learned"              => '"what did you learn?"',
-        "description"          => "situation",
-        "support_wp"           => '"what do you need from your workplace?"',
-        "support_ed"           => '"what do you need from the HU?"',
-        "learning_goal"        => 'learning goal',
-        "competence"           => "competence",
-        "date"                 => "date",
-        "res_material_detail"  => "theory source",
-        "timeslot"             => "timeslot",
-        "companyName"          => "company name",
-        'companyStreet'        => 'street',
-        'companyHousenr'       => 'house number',
-        'companyPostalcode'    => 'postal code',
-        'companyLocation'      => 'location',
-        'contactPerson'        => 'contact person',
-        'contactPhone'         => 'phone number',
-        'contactEmail'         => 'email address',
-        'numdays'              => 'number of days',
-        'startdate'            => 'planned start date',
-        'enddate'              => 'planned end date',
-        'internshipAssignment' => 'internship assignment',
-        'isActive'             => 'current internship',
-        'nameDeadline'         => 'deadline description',
-        'dateDeadline'         => 'deadline date',
-        'firstname'            => 'first name',
-        'lastname'             => 'last name',
-        'email'                => 'email address',
-
-    ],
-
-];
+return array (
+  'accepted' => 'The :attribute must be accepted.',
+  'active_url' => 'The :attribute is not a valid URL.',
+  'after' => 'The :attribute must be a date after :date.',
+  'alpha' => 'The :attribute may only contain letters.',
+  'alpha_dash' => 'The :attribute may only contain letters, numbers, and dashes.',
+  'alpha_num' => 'The :attribute may only contain letters and numbers.',
+  'array' => 'The :attribute must be an array.',
+  'before' => 'The :attribute must be a date before :date.',
+  'between' => 
+  array (
+    'numeric' => 'The :attribute must be between :min and :max.',
+    'file' => 'The :attribute must be between :min and :max kilobytes.',
+    'string' => 'The :attribute must be between :min and :max characters.',
+    'array' => 'The :attribute must have between :min and :max items.',
+  ),
+  'boolean' => 'The :attribute field must be true or false.',
+  'confirmed' => 'The :attribute confirmation does not match.',
+  'date' => 'The :attribute is not a valid date.',
+  'date_format' => 'The :attribute does not match the format :format.',
+  'different' => 'The :attribute and :other must be different.',
+  'digits' => 'The :attribute must be :digits digits.',
+  'digits_between' => 'The :attribute must be between :min and :max digits.',
+  'dimensions' => 'The :attribute has invalid image dimensions.',
+  'distinct' => 'The :attribute field has a duplicate value.',
+  'email' => 'The :attribute must be a valid email address.',
+  'exists' => 'The selected :attribute is invalid.',
+  'file' => 'The :attribute must be a file.',
+  'filled' => 'The :attribute field is required.',
+  'image' => 'The :attribute must be an image.',
+  'in' => 'The selected :attribute is invalid.',
+  'in_array' => 'The :attribute field does not exist in :other.',
+  'integer' => 'The :attribute must be an integer.',
+  'ip' => 'The :attribute must be a valid IP address.',
+  'json' => 'The :attribute must be a valid JSON string.',
+  'max' => 
+  array (
+    'numeric' => 'The :attribute may not be greater than :max.',
+    'file' => 'The :attribute may not be greater than :max kilobytes.',
+    'string' => 'The :attribute may not be greater than :max characters.',
+    'array' => 'The :attribute may not have more than :max items.',
+  ),
+  'mimes' => 'The :attribute must be a file of type: :values.',
+  'mimetypes' => 'The :attribute must be a file of type: :values.',
+  'min' => 
+  array (
+    'numeric' => 'The :attribute must be at least :min.',
+    'file' => 'The :attribute must be at least :min kilobytes.',
+    'string' => 'The :attribute must be at least :min characters.',
+    'array' => 'The :attribute must have at least :min items.',
+  ),
+  'not_in' => 'The selected :attribute is invalid.',
+  'numeric' => 'The :attribute must be a number.',
+  'present' => 'The :attribute field must be present.',
+  'regex' => 'The :attribute format is invalid.',
+  'required' => 'The :attribute field is required.',
+  'required_if' => 'The :attribute field is required when :other is :value.',
+  'required_unless' => 'The :attribute field is required unless :other is in :values.',
+  'required_with' => 'The :attribute field is required when :values is present.',
+  'required_with_all' => 'The :attribute field is required when :values is present.',
+  'required_without' => 'The :attribute field is required when :values is not present.',
+  'required_without_all' => 'The :attribute field is required when none of :values are present.',
+  'same' => 'The :attribute and :other must match.',
+  'size' => 
+  array (
+    'numeric' => 'The :attribute must be :size.',
+    'file' => 'The :attribute must be :size kilobytes.',
+    'string' => 'The :attribute must be :size characters.',
+    'array' => 'The :attribute must contain :size items.',
+  ),
+  'string' => 'The :attribute must be a string.',
+  'timezone' => 'The :attribute must be a valid zone.',
+  'unique' => 'The :attribute has already been taken.',
+  'uploaded' => 'The :attribute failed to upload.',
+  'url' => 'The :attribute format is invalid.',
+  'custom' => 
+  array (
+    'res_person' => 
+    array (
+      'required' => 'The "with whom?" field is required.',
+      'exists' => 'This "with whom?" does not exist.',
+    ),
+    'new_rp' => 
+    array (
+      'required' => 'The description for the "with whom?" field is required.',
+      'max' => 'The description for the "with whom?" field can only be 45 characters long.',
+      'regex' => 'The description for the "with whom?" field has an invalid format.',
+    ),
+    'res_material' => 
+    array (
+      'required' => 'The "with which theory?" field is required.',
+      'exists' => 'This "with which theory?" does not exist.',
+    ),
+    'new_rm' => 
+    array (
+      'required' => 'The description for the "with which theory?" field is required.',
+      'max' => 'The description for the "with which theory?" field  can only be 45 characters long.',
+      'regex' => 'The description for the "with which theory?" field  has an invalid format.',
+    ),
+    'competence' => 
+    array (
+      'exists' => 'The selected :attribute is invalid',
+    ),
+    'learning_goal' => 
+    array (
+      'exists' => 'The selected :attribute is invalid',
+    ),
+    'res_material_detail' => 
+    array (
+      'required_unless' => 'The :attribute field is required.',
+    ),
+  ),
+  'attributes' => 
+  array (
+    'learned' => '"what did you learn?"',
+    'description' => 'situation',
+    'support_wp' => '"what do you need from your workplace?"',
+    'support_ed' => '"what do you need from the HU?"',
+    'learning_goal' => 'learning goal',
+    'competence' => 'competence',
+    'date' => 'date',
+    'res_material_detail' => 'theory source',
+    'timeslot' => 'timeslot',
+    'companyName' => 'company name',
+    'companyStreet' => 'street',
+    'companyHousenr' => 'house number',
+    'companyPostalcode' => 'postal code',
+    'companyLocation' => 'location',
+    'contactPerson' => 'contact person',
+    'contactPhone' => 'phone number',
+    'contactEmail' => 'email address',
+    'numdays' => 'number of days',
+    'startdate' => 'planned start date',
+    'enddate' => 'planned end date',
+    'internshipAssignment' => 'internship assignment',
+    'isActive' => 'current internship',
+    'nameDeadline' => 'deadline description',
+    'dateDeadline' => 'deadline date',
+    'firstname' => 'first name',
+    'lastname' => 'last name',
+    'email' => 'email address',
+  ),
+  'after_or_equal' => 'The :attribute should be a date equal to or after :date',
+  'before_or_equal' => 'The :attribute should be a date equal to or before :date',
+);
