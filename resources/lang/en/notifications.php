5596c210c450eac9da0be6c3229eed9d79360947
Rogier Knoester <knoesterrogier@gmail.com>
2017-10-26 11:47:08 +0200

    fixes #117

---
+++ resources/lang/en/notifications.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  'generic' => 
+  array (
+    'nointernshipactive' => 'You currently do not have an active internship. Click <a href="%s">here</a> to create one.',
+      "nointernshipprogress" => "You cannot see any progress until you have added an internship."
+  ),
+);
--- resources/lang/nl/notifications.php
+++ resources/lang/nl/notifications.php
@@ -4,5 +4,6 @@ return array (
   'generic' => 
   array (
     'nointernshipactive' => 'Je hebt momenteel geen actieve stage. Klik <a href="%s">hier</a> om er een toe te voegen.',
+      "nointernshipprogress" => "Je kunt je voortgang nog niet bekijken want je hebt nog geen werkplek."
   ),
 );
