be3750e8d673724f54cf1853a1a50795e4749659
Rogier Knoester <knoesterrogier@gmail.com>
2017-11-21 16:03:09 +0100

    fix exportbuilder test

--- tests/Unit/LearningActivityExportBuilderTest.php
+++ tests/Unit/LearningActivityExportBuilderTest.php
@@ -5,9 +5,6 @@ namespace Tests\Feature;
 use App\LearningActivityActing;
 use App\LearningActivityActingExportBuilder;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LearningActivityExportBuilderTest extends TestCase
 {
@@ -15,22 +12,34 @@ class LearningActivityExportBuilderTest extends TestCase
     private function buildMock() {
 
         $mock = \Mockery::mock(LearningActivityActing::class);
-//        $mock->date = '12-12';
+
         $mock->shouldReceive('getAttribute')->with('id')->andReturn('1');
-        $mock->shouldReceive('getAttribute')->with('date')->andReturn('12-12');
+        $mock->shouldReceive('getAttribute')->with('date')->andReturn('2017-10-10');
         $mock->shouldReceive('getAttribute')->with('situation')->andReturn('pressure');
-//        $mock->situation = "pressure";
+
         $mock->shouldReceive('getTimeslot')->times(1)->andReturn('1e lesuur');
         $mock->shouldReceive('getResourcePerson')->times(1)->andReturn('Medestudent');
         $mock->shouldReceive('getResourceMaterial')->times(1)->andReturn('Geen');
         $mock->shouldReceive('getAttribute')->with('lessonslearned')->andReturn('a lot');
-//        $mock->lessonslearned = "a lot";
         $mock->shouldReceive('getLearningGoal')->times(1)->andReturn('Leervraag 1');
+
+        $learningGoal = new \StdClass;
+        $learningGoal->description = "Description test";
+        $mock->shouldReceive('getAttribute')->with('learningGoal')->times(1)->andReturn($learningGoal);
+
+        $mock->shouldReceive('getAttribute')->with('support_wp')->andReturn('support from wp');
+        $mock->shouldReceive('getAttribute')->with('support_ed')->andReturn('support from ed');
+
         $competenceObject = new \StdClass;
         $competenceObject->competence_label = "Interpersoonlijk";
         $mock->shouldReceive('getCompetencies')->times(1)->andReturn($competenceObject);
         $mock->shouldReceive('getAttribute')->with('laa_id')->andReturn('1');
+
+        $mock->shouldReceive('getAttribute')->with('evidence_filename')->andReturn(null);
+
         return $mock;
+
+
     }
 
     /**
@@ -49,16 +58,20 @@ class LearningActivityExportBuilderTest extends TestCase
 
         $mapping = [
             "id" => 1,
-            "date" => "12-12",
+            "date" => "10-10-2017",
             "situation" => "pressure",
             "timeslot" => "1e lesuur",
             "resourcePerson" => "Medestudent",
             "resourceMaterial" => "Geen",
             "lessonsLearned" => "a lot",
             "learningGoal" => "Leervraag 1",
+            "learningGoalDescription" => "Description test",
+            "supportWp" => "support from wp",
+            "supportEd" => "support from ed",
             "competence" => "Interpersoonlijk",
             "url" => route('process-acting-edit', ["id" => 1])
         ];
+
 
         foreach($mapping as $field => $value) {
             $this->assertEquals($value, $decoded[0]->{$field}, "{$field}: expected({$value}) got({$decoded[0]->{$field}})");
