c80f6d8bb339a965ac2a8296036e32c1d7c40177
Rogier Knoester <knoesterrogier@gmail.com>
2018-01-19 15:40:07 +0100

    english statistic lang

---
+++ resources/lang/en/statistics.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "create-new" => "Create new statistic",
+    "statistic" => "Statistic",
+    "all-statistics" => "All statistics",
+    "go-to" => "Go to statistics",
+    "is-nested-by-others" => "This statistic cannot be removed because it is used by other statistics: :statistics"
+];
