9f448aa0c18dc3c279648d1a5214d598a4412a6e
Rogier Knoester <knoesterrogier@gmail.com>
2018-02-06 16:19:31 +0100

    more colors for chart

--- resources/views/pages/charts/show.blade.php
+++ resources/views/pages/charts/show.blade.php
@@ -25,7 +25,8 @@
     </div>
     <script>
       (function () {
-        var ctx = $('#myChart')
+
+          var ctx = $('#myChart')
         var myChart = new Chart(ctx, {
           type: '{{ $chart->type->slug }}', // ideally have the type itself make something nice out?
           data: {
@@ -38,12 +39,24 @@
             datasets: [{
               label: '{{ $chart->label }}',
               backgroundColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
               ],
               data: [<?php
                       $x_items = array_map(function ($key) use ($chart){
