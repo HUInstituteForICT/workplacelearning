9ff714c0f4661a417bef18b658c13c0c46130a26
Rogier Knoester <knoesterrogier@gmail.com>
2018-02-26 16:00:14 +0100

    fix migration null default value

--- database/migrations/2018_02_26_144716_AddCountryToWorkplace.php
+++ database/migrations/2018_02_26_144716_AddCountryToWorkplace.php
@@ -14,7 +14,7 @@ class AddCountryToWorkplace extends Migration
     public function up()
     {
         Schema::table('workplace', function(Blueprint $table) {
-            $table->string('country');
+            $table->string('country')->default('');
         });
     }
 
