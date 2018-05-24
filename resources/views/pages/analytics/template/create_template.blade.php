a3dc992725f272de1619fc734b882d3a222a8a20
WhiteWalker72 <sivarwerrie@hotmail.nl>
2018-05-24 11:39:58 +0200

    Tiles updated

--- resources/views/pages/analytics/template/create_template.blade.php
+++ resources/views/pages/analytics/template/create_template.blade.php
@@ -1,37 +1,39 @@
 @extends('layout.HUdefault')
 @section('title', 'Template maken')
 @section('content')
-    <div class="row">
-        <div class="col-md-6">
-            <h1>{{Lang::get('template.template_create')}}</h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>{{Lang::get('template.template_create')}}</h1>
 
-            <form action="{{ route('template.save') }}" method="post" id="saveForm">
+                <form action="{{ route('template.save') }}" method="post" id="saveForm">
 
-                <div class="form-group">
-                    <label for="name">{{Lang::get('template.name')}}</label>
-                    <input type="text" id="name" name="name" class="form-control">
-                </div>
+                    <div class="form-group">
+                        <label for="name">{{Lang::get('template.name')}}</label>
+                        <input type="text" id="name" name="name" class="form-control">
+                    </div>
 
-                <div class="form-group">
-                    <label for="query">Query</label>
-                    <textarea rows="4" cols="50" onkeyup="loadParams()" maxlength="100" id="query" name="query"
-                              class="form-control"></textarea>
-                </div>
+                    <div class="form-group">
+                        <label for="query">Query</label>
+                        <textarea rows="4" cols="50" onkeyup="loadParams()" maxlength="1000" id="query" name="query"
+                                  class="form-control"></textarea>
+                    </div>
 
-                <div class="form-group" id="paramGroup">
-                    {{--JS will load parameters here--}}
-                </div>
+                    <div class="form" id="paramGroup">
+                        {{--JS will load parameters here--}}
+                    </div>
 
-                <button class="btn btn-primary" style="float: right" type="submit"
-                        title="Opslaan">{{Lang::get('template.save')}}</button>
-                {{ csrf_field() }}
-            </form>
+                    <button class="btn btn-primary" style="float: right" type="submit"
+                            title="Opslaan">{{Lang::get('template.save')}}</button>
+                    {{ csrf_field() }}
+                </form>
 
-            <form action="{{ route('template.index') }}" method="get">
-                <button class="btn btn-default" title="Terug">{{Lang::get('template.back')}}
-                </button>
-            </form>
+                <form action="{{ route('template.index') }}" method="get">
+                    <button class="btn btn-default" title="Terug">{{Lang::get('template.back')}}
+                    </button>
+                </form>
 
+            </div>
         </div>
     </div>
 
@@ -54,11 +56,6 @@
                     if (elements != null) {
                         elements.forEach(elem => paramGroup.removeChild(elem));
                     }
-
-                    let selectLists = document.getElementsByName(name + "list");
-                    if (selectLists != null) {
-                        selectLists.forEach(list => paramGroup.removeChild(list));
-                    }
                 }
 
                 lastCount = count;
@@ -68,15 +65,12 @@
                     let inputField = document.createElement("input");
                     inputField.setAttribute("type", "text");
                     inputField.setAttribute("placeholder", "param " + (i + 1));
-                    inputField.setAttribute("name", name);
                     inputField.setAttribute("id", name);
                     inputField.setAttribute("required", "true");
-                    inputField.style.marginTop = "6px";
                     inputField.style.cssFloat = "left";
                     inputField.className = "form-control";
 
                     let selectList = document.createElement("select");
-                    selectList.setAttribute("name", name + "list");
                     selectList.className = "form-control";
                     selectList.style.cssFloat = "right";
                     selectList.style.marginTop = "2px";
@@ -88,11 +82,31 @@
                         selectList.appendChild(option);
                     }
 
-                    document.getElementById('paramGroup').appendChild(inputField);
-                    document.getElementById('paramGroup').appendChild(selectList);
+                    let row = document.createElement("div");
+                    row.setAttribute("name", name);
+                    row.className = "row";
+
+                    let div1 = getDiv();
+                    div1.appendChild(inputField);
+
+                    let div2 = getDiv();
+                    div2.appendChild(selectList);
+
+                    row.append(div1);
+                    row.append(div2);
+                    row.append(getDiv());
+
+                    document.getElementById('paramGroup').appendChild(row);
                 }
             }
         }
+
+        function getDiv() {
+            let div = document.createElement("div");
+            div.className = "col-md-3";
+            return div;
+        }
+
     </script>
 
 @stop
