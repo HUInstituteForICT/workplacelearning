b71b421d7ac2adee2b76642384195a38cbcf4811
Briggy238 <brigitte.leblanc@student.hu.nl>
2018-06-14 11:44:39 +0200

    Added modal-body

--- resources/views/pages/analytics/builder/step2-custom.blade.php
+++ resources/views/pages/analytics/builder/step2-custom.blade.php
@@ -2,13 +2,11 @@
     <button type="button" class="close" data-dismiss="modal">&times;</button>
     <h4 class="modal-title">@lang('querybuilder.step2.custom')</h4>
 </div>
+<div class="modal-body" style="height: 450px">
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-9">
-            <h1>{{Lang::get('custom_query.custom_query')}}</h1>
-
             <form id="wizard-form">
-
                 <div class="form-group">
                     <label for="Query">{{Lang::get('custom_query.query')}}</label>
                     <textarea rows="4" cols="50" maxlength="1000" id="customQuery" name="customQuery"
@@ -22,13 +20,11 @@
                     <label for="dataFromQuery">{{Lang::get('custom_query.dataQuery')}}</label>
                     <div id="dataCustomQuery"></div>
                 </div>
-
-                <!--https://stackoverflow.com/questions/5207160/what-is-a-csrf-token-what-is-its-importance-and-how-does-it-work/33829607#33829607-->
-                {{ csrf_field() }}
             </form>
 
         </div>
     </div>
+</div>
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-secondary" onclick="Wizard.step(1);">Vorige</button>
