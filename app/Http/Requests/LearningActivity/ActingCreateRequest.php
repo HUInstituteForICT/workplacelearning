5786a811f0787bc5980ca52e1aa0dd1027f7f886
Rogier Knoester <knoesterrogier@gmail.com>
2018-10-03 14:42:33 +0200

    Acting activity description/situation max character length 1000 -> 1500

--- app/Http/Requests/LearningActivity/ActingCreateRequest.php
+++ app/Http/Requests/LearningActivity/ActingCreateRequest.php
@@ -16,7 +16,7 @@ class ActingCreateRequest extends FormRequest
     {
         return [
             'date' => 'required|date|date_in_wplp',
-            'description' => 'required|max:1000',
+            'description' => 'required|max:1500',
             'learned' => 'required|max:1000',
             'support_wp' => 'max:500',
             'support_ed' => 'max:500',
@@ -29,27 +29,27 @@ class ActingCreateRequest extends FormRequest
     public function withValidator(Validator $validator): void
     {
         $validator->sometimes('res_person', 'required|exists:resourceperson,rp_id', function ($input) {
-            return 'new' !== $input->res_person;
+            return $input->res_person !== 'new';
         });
 
         $validator->sometimes('timeslot', 'required|exists:timeslot,timeslot_id', function ($input) {
-            return 'new' !== $input->timeslot;
+            return $input->timeslot !== 'new';
         });
 
         $validator->sometimes('res_material', 'required|exists:resourcematerial,rm_id', function ($input) {
-            return 'new' !== $input->res_material && 'none' !== $input->res_material;
+            return $input->res_material !== 'new' && $input->res_material !== 'none';
         });
 
         $validator->sometimes('new_rp', 'required|max:45', function ($input) {
-            return 'new' === $input->res_person;
+            return $input->res_person === 'new';
         });
 
         $validator->sometimes('new_timeslot', 'required|max:45', function ($input) {
-            return 'new' === $input->timeslot;
+            return $input->timeslot === 'new';
         });
 
         $validator->sometimes('new_rm', 'required|max:45', function ($input) {
-            return 'new' === $input->res_material;
+            return $input->res_material === 'new';
         });
 
         $validator->sometimes('res_material_detail', 'required_unless:res_material,none|max:75', function ($input) {
---
+++ database/migrations/2018_10_03_143512_LAASituationCharacterLength.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LAASituationCharacterLength extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('learningactivityacting', function (Blueprint $table) {
+            $table->string('situation', 1500)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+    }
+}
