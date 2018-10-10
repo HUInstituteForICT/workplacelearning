9aefd6049f3a1a897d5ac19b2bf3ea1e7c00b8fc
Rogier Knoester <knoesterrogier@gmail.com>
2018-10-10 11:44:23 +0200

    add spatie/laravel-translation-loader

---
+++ database/migrations/2018_10_10_114052_create_language_lines_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguageLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('language_lines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('group');
+            $table->index('group');
+            $table->string('key');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('language_lines');
+    }
+}
