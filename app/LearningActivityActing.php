03bb0da4cd138aca42d46b8a552b55cdcb6cec7c
Rogier Knoester <knoesterrogier@gmail.com>
2018-12-04 15:05:56 +0100

    fix date issue in workplaceperiod forms

--- app/LearningActivityActing.php
+++ app/LearningActivityActing.php
@@ -61,10 +61,6 @@ class LearningActivityActing extends Model implements LearningActivityInterface
     // Override the primary key column
     protected $primaryKey = 'laa_id';
 
-    protected $casts = [
-        'date' => 'date',
-    ];
-
     // Default
     protected $fillable = [
         'laa_id',
