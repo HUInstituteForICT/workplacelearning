f4c0c71b3e7798c88ce6785c87578b3ea190ac5a
Rogier Knoester <knoesterrogier@gmail.com>
2019-01-12 16:28:25 +0100

    in periodmomentcalculator defer loading of period until actually needed because sometimes the user is not initialised yet

--- tests/Unit/Tips/PeriodMomentCalculatorTest.php
+++ tests/Unit/Tips/PeriodMomentCalculatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Unit\Tips;
 
+use App\Services\CurrentPeriodResolver;
 use App\Tips\PeriodMomentCalculator;
 use App\WorkplaceLearningPeriod;
 use Carbon\Carbon;
@@ -21,7 +22,10 @@ class PeriodMomentCalculatorTest extends TestCase
         $wplp->startdate = $start->format('Y-m-d');
         $wplp->enddate = $end->format('Y-m-d');
 
-        $periodMomentCalculator = new PeriodMomentCalculator($wplp);
+        $resolver = $this->createMock(CurrentPeriodResolver::class);
+        $resolver->expects(self::once())->method('getPeriod')->willReturn($wplp);
+
+        $periodMomentCalculator = new PeriodMomentCalculator($resolver);
 
         $this->assertEquals('50', $periodMomentCalculator->getMomentAsPercentage());
     }
