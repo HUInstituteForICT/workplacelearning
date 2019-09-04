600e1c1fe91873790eeca6be2b80bc284fc97838
Rogier <knoesterrogier@gmail.com>
2019-09-04 12:30:10 +0200

    add js urls on acting progress

--- resources/views/pages/acting/progress.blade.php
+++ resources/views/pages/acting/progress.blade.php
@@ -23,6 +23,8 @@
                 window.activities = {!! $activitiesJson !!};
                 window.exportTranslatedFieldMapping = {!! $exportTranslatedFieldMapping !!};
                 window.reflectionDownloadMultipleUrl = '{{ route('reflection-download-multiple') }}';
+                window.exportActivitiesUrl = '{{ route('acting-activities-word-export') }}';
+                window.mailExportActivitiesUrl = '{{ route('mail-acting-activities-word-export') }}';
             </script>
 
             <div id="ActivityActingProcessTable" class="__reactRoot col-md-12"></div>
