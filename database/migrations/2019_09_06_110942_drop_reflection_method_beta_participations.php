ace2d885c9d10315a35523fd234ffe726b83c69a
Rogier <knoesterrogier@gmail.com>
2019-09-06 11:14:45 +0200

    remove reflection method beta participations table

---
+++ database/migrations/2019_09_06_110942_drop_reflection_method_beta_participations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropReflectionMethodBetaParticipations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('reflection_method_beta_participations');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::create('reflection_method_beta_participations', static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id')->nullable(false);
+            $table->boolean('participates')->nullable(false)->default(false);
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+
+        Schema::table('reflection_method_beta_participations', static function (Blueprint $table) {
+            $table->foreign('student_id', 'optin_to_student_id')
+                ->references('student_id')->on('student')->onDelete('CASCADE');
+        });
+    }
+}
