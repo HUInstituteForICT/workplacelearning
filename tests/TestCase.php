0437f05d5c741568b7385a4811101732efc8d169
Rogier Knoester <knoesterrogier@gmail.com>
2019-11-21 13:37:30 +0100

    use custom database migration strategy on tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabaseState;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -12,4 +14,18 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
 
     use DatabaseMigrations;
+
+    /**
+     * Define hooks to migrate the database before and after each test.
+     */
+    public function runDatabaseMigrations(): void
+    {
+        $this->artisan('migrate:fresh');
+
+        $this->app[Kernel::class]->setArtisan(null);
+
+        $this->beforeApplicationDestroyed(function () {
+            RefreshDatabaseState::$migrated = false;
+        });
+    }
 }
