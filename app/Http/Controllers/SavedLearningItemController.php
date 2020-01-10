bfe0df3b70fcc5913b8dd08a4c4995b024043186
ElineNube <31698656+ElineNube@users.noreply.github.com>
2020-01-10 11:31:04 +0100

    Add feature to remove a tip from a folder

--- app/Http/Controllers/SavedLearningItemController.php
+++ app/Http/Controllers/SavedLearningItemController.php
@@ -86,10 +86,12 @@ class SavedLearningItemController extends Controller
     public function updateFolder(Request $request)
     {
         $savedLearningItem =  $this->savedLearningItemRepository->findById($request['sli_id']);
-        $folderId = $request['chooseFolder'];
-        $savedLearningItem->folder = $folderId;
+        if($request['chooseFolder'] == 0){
+            $savedLearningItem->folder = null;
+        }else{
+            $savedLearningItem->folder =  $request['chooseFolder'];
+        }
         $savedLearningItem->save();
-
         return redirect('saved-learning-items');
     }
 }
