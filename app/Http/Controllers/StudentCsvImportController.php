4c63e4450579b8a83370f00853518be240b1289c
TimvanderMeys <tim.vandermeijs@student.hu.nl>
2020-03-18 16:27:20 +0100

    Added Controller and View

--- app/Http/Controllers/StudentCsvImportController.php
+++ app/Http/Controllers/StudentCsvImportController.php
@@ -2,73 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\Factories\LAPFactory;
 use Illuminate\Http\Request;
+use function Sodium\add;
 
 class StudentCsvImportController extends Controller
 {
-    public function show(Request $request) {
-
-        return view('pages.producing.activity-import');
-    }
-
-    public function save(Request $request,
-                         LAPFactory $LAPFactory
-                        )
+    public function save(Request $request)
     {
-        $request->validate(["csv_file" => 'required']);
-
         if($request->hasFile('file'))
         {
             $filepath = $request->file('file')->getRealPath();
             $file = fopen($filepath, "r");
-            $count = 0;
+            $dumpArray = [];
 
             while (($getData = fgetcsv($file, ",")) !== FALSE)
             {
-                    if ($count >= 5 && !$getData[0] == "" && !$getData[1] == "") {
-                        $data = [];
+                if (!$getData[0] == "" && !$getData[1] == "")
+                {
+                    $getData[0] =
+                    $getData[1] =
+                    $getData[2] =
+                    $getData[3] =
+                    $getData[4] =
+                    $getData[5] =
+                    $getData[6] =
 
-                        $data['category_id'] = $getData[3];
-                        $data['omschrijving'] = $getData[1];
-                        $data['aantaluren'] = $getData[2];
-                        $data['aantaluren_custom'] = $getData[2] * 60;
-                        $data['datum'] = $getData[0];
-                        $data['extrafeedback'] = '';
 
-                        switch($getData[6]) {
-                            case 'Makkelijk':
-                                $data['moeilijkheid'] = 1;
-                                break;
-                            case 'Gemiddeld':
-                                $data['moeilijkheid'] = 2;
-                                break;
-                            case 'Moeilijk':
-                                $data['moeilijkheid'] = 3;
-                                break;
-                        }
-
-                        $data['status'] = $getData[5];
-
-                        if(strtolower(substr($getData[4], 0, 7)) === 'persoon') {
-                            $resourceExplode = explode('- ', $getData[4]);
-                            $data['resource'] = $resourceExplode[0];
-                            $data['resource_person_id'] = $resourceExplode[1];
-                        }
-                        else {
-                            $data['resource'] = $getData[4];
-                        }
-
-                        $data['internetsource'] = '';
-                        $data['booksource'] = '';
-                        $data['chain_id'] = '';
-
-                        $LAPFactory->createLAP($data);
-                    }
-                $count++;
+                    array_push($dumpArray, $getData);
+                }
             }
         }
+        dd($dumpArray);
 
-        return view('pages.producing.activity-import')->with('successMsg', 'works');
+        return view('pages.admin.dd-import');
         }
 }
