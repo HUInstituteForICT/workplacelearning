3f40883528eccd9cee006337007b9c2d91e4392c
santinodenbrave <santinodenbrave@protonmail.com>
2020-03-18 16:59:21 +0100

    voorbeeld data saven

--- app/Http/Controllers/StudentCsvImportController.php
+++ app/Http/Controllers/StudentCsvImportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use function Sodium\add;
+use App\Student;
 
 class StudentCsvImportController extends Controller
 {
@@ -14,22 +15,37 @@ class StudentCsvImportController extends Controller
             $filepath = $request->file('file')->getRealPath();
             $file = fopen($filepath, "r");
             $dumpArray = [];
+            $count = 0;
+
+            $student = new Student();
 
             while (($getData = fgetcsv($file, ",")) !== FALSE)
             {
-                if (!$getData[0] == "" && !$getData[1] == "")
-                {
-                    $getData[0] =
-                    $getData[1] =
-                    $getData[2] =
-                    $getData[3] =
-                    $getData[4] =
-                    $getData[5] =
-                    $getData[6] =
+                if(count >= 4) {
+                    if (!$getData[0] == "" && !$getData[1] == "") {
 
 
-                    array_push($dumpArray, $getData);
+                        // Difficulty, Category, Status, Duration zijn verplichte Foreign Keys
+                        // Bovenstaande moeten eerst ingeschoten worden voordat date kan.
+
+                        $date = $getData[0];
+                        $timestamp = strtotime($date);
+
+//                        $student->save($timestamp);
+
+
+                        //                    $getData[1] =
+                        //                    $getData[2] =
+                        //                    $getData[3] =
+                        //                    $getData[4] =
+                        //                    $getData[5] =
+                        //                    $getData[6] =
+
+
+                        array_push($dumpArray, $getData);
+                    }
                 }
+                $count++;
             }
         }
         dd($dumpArray);
