aa0a23e4527458c79303e3383f07dc4e372746b7
TimvanderMeys <tim.vandermeijs@student.hu.nl>
2020-03-26 10:59:14 +0100

    ...

--- app/Http/Controllers/StudentCsvImportController.php
+++ app/Http/Controllers/StudentCsvImportController.php
@@ -4,112 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Services\Factories\LAPFactory;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use function Sodium\add;
 
 class StudentCsvImportController extends Controller
 {
-    public function show(Request $request) {
-
-        return view('pages.producing.activity-import');
-    }
-
-    public function save(Request $request)
+    public function save(Request $request,
+                         LAPFactory $LAPFactory
+    )
     {
-
-        $request->validate([
-            "csv_file" => 'required',
-        ]);
-
-        return view('pages.producing.activity-import')->with('successMsg', 'works');
+        $request->validate(["csv_file" => 'required']);
 
         if($request->hasFile('file'))
         {
             $filepath = $request->file('file')->getRealPath();
             $file = fopen($filepath, "r");
-            $dumpArray = [];
             $count = 0;
-
-            $lapFactory = new LAPFactory();
 
             while (($getData = fgetcsv($file, ",")) !== FALSE)
             {
-                if($count >= 5) {
-                    if (!$getData[0] == "" && !$getData[1] == "") {
+                if ($count >= 5 && !$getData[0] == "" && !$getData[1] == "") {
+                    $data = [];
 
+                    $data['category_id'] = $getData[3];
+                    $data['omschrijving'] = $getData[1];
+                    $data['aantaluren'] = $getData[2];
+                    $data['aantaluren_custom'] = $getData[2] * 60;
+                    $data['datum'] = $getData[0];
+                    $data['extrafeedback'] = '';
 
-                        // Difficulty, Category, Status, Duration zijn verplichte Foreign Keys
-                        // Bovenstaande moeten eerst ingeschoten worden voordat date kan.
-
-
-                        $timestamp = strtotime($getData[0]);
-                        $omschrijving = $getData[1];
-                        $duur = $getData[2];
-                        $categorie = $getData[3];
-                        $werkenLerenMet = $getData[4];
-                        $status = $getData[5];
-                        $moeilijkheidsgraad = $getData[6];
-//
-//                        dd($timestamp,
-//                            $omschrijving,
-//                            $duur,
-//                            $categorie,
-//                            $werkenLerenMet,
-//                            $status,
-//                            $moeilijkheidsgraad
-//                        );
-
-
-
-                        switch ($categorie) {
-                            case 'Onderzoek':
-                                // TODO: Implement a check to see which cohort_id it is.
-                                // For example:
-                                // if(cohort_id = 40){ $category = 834}
-                                // if(cohort_id = 41) {$category = 849};
-                                $categorie = 834;
-                                break;
-                            case 'ICT-Documentatie':
-                                $categorie = 835;
-                                break;
-                            case 'Schooldocumentatie':
-                                $categorie = 836;
-                                break;
-                            case 'Overleg':
-                                $categorie = 837;
-                                break;
-                            case 'Programmeren':
-                                $categorie = 846;
-                                break;
-                            case 'Analyseren & Ontwerpen':
-                                $categorie = 847;
-                                break;
-                            case 'Testen':
-                                $categorie = 848;
-                                break;
-                        }
-
-
-
-//                        $student->save($timestamp);
-
-
-                        //                    $getData[1] =
-                        //                    $getData[2] =
-                        //                    $getData[3] =
-                        //                    $getData[4] =
-                        //                    $getData[5] =
-                        //                    $getData[6] =
-
-
-//                        array_push($dumpArray, $getData);
+                    switch($getData[6]) {
+                        case 'Makkelijk':
+                            $data['moeilijkheid'] = 1;
+                            break;
+                        case 'Gemiddeld':
+                            $data['moeilijkheid'] = 2;
+                            break;
+                        case 'Moeilijk':
+                            $data['moeilijkheid'] = 3;
+                            break;
                     }
+
+                    $data['status'] = $getData[5];
+
+                    if(strtolower(substr($getData[4], 0, 7)) === 'persoon') {
+                        $resourceExplode = explode('- ', $getData[4]);
+                        $data['resource'] = $resourceExplode[0];
+                        $data['resource_person_id'] = $resourceExplode[1];
+                    }
+                    else {
+                        $data['resource'] = $getData[4];
+                    }
+
+                    $data['internetsource'] = '';
+                    $data['booksource'] = '';
+                    $data['chain_id'] = '';
+
+                    $LAPFactory->createLAP($data);
                 }
                 $count++;
             }
         }
-        dd($dumpArray);
 
-        return view('pages.admin.dd-import');
-        }
+        return view('pages.producing.activity-import')->with('successMsg', 'works');
+    }
 }
