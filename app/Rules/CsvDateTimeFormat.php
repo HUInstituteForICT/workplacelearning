8932c7dd92a672cea0bed744e8ea5eef9acb8999
TimvanderMeys <tim.vandermeijs@student.hu.nl>
2020-04-02 13:13:11 +0200

    Added CsvDateTimeFormat rule

---
+++ app/Rules/CsvDateTimeFormat.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use DateTime;
+use Illuminate\Contracts\Validation\Rule;
+
+class csvDateTimeFormat implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if(DateTime::createFromFormat('d/m/Y', $value)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Datum incorrect geformatteerd, moet zijn: dag/maand/jaar';
+    }
+}
