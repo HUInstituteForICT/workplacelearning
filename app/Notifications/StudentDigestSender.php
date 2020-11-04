0cdb931bb3fcd249be7f01048edd9176536da063
Douwe Hoogeveen <info@wakkermedia.nl>
2020-11-04 10:53:25 +0100

    Solved issue with type of array for notifications

--- app/Notifications/StudentDigestSender.php
+++ app/Notifications/StudentDigestSender.php
@@ -25,19 +25,14 @@ class StudentDigestSender
     public function sendNotifications(array $notifications, Student $user): void
     {
 
-        // Disable function usableNotifications because it fails. Not sure why it is needed!
         // $usableNotifications = array_filter($notifications, function (DatabaseNotification $notification) {
         //     return $notification->type === FolderFeedbackGiven::class;
         // });
-        
-        // foreach ($usableNotifications as $notification) {
-        //     $notification->markAsRead();
-        // }
-        
-        // Instead of checking if the notification is usable, use the original notification.
+
         $this->mailer->to($user->email)->send(new StudentDigest($user, $notifications));
+
         foreach ($notifications as $notification) {
             $notification->markAsRead();
         }
-
-    }}
+    }
+}
