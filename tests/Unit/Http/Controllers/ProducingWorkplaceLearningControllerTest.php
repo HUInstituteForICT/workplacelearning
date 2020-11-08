fde63b06905c43e1ae5ef5553ceb385a0bc030ef
Remco <remcotaal@hotmail.com>
2020-11-08 18:51:28 +0100

    Add TODO for ProducingWorkplaceLearningControllerTest

---
+++ tests/Unit/Http/Controllers/ProducingWorkplaceLearningControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Http\Controllers;
+
+use Tests\TestCase;
+
+class ProducingWorkplaceLearningControllerTest extends TestCase
+{
+    public function testShow(): void
+    {
+       //TODO implement show unit test
+        $this->assertTrue(true);
+    }
+
+    public function testEdit(): void
+    {
+        //TODO implement edit unit test
+        $this->assertTrue(true);
+    }
+
+    public function testCreate(): void
+    {
+        //TODO implement create unit test
+        $this->assertTrue(true);
+    }
+
+    public function testUpdate(): void
+    {
+        //TODO implement update unit test
+        $this->assertTrue(true);
+    }
+
+    public function testUpdateCategories(): void
+    {
+        //TODO implement updateCategories unit test
+        $this->assertTrue(true);
+    }
+}
