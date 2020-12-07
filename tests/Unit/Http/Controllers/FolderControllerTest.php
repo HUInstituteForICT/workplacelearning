c8e78236bbb09ff70dde93656566691dfe9e15f4
Bart <bart26nl@gmail.com>
2020-12-07 14:22:07 +0100

    empty test.

---
+++ tests/Unit/Http/Controllers/FolderControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Http\Controllers;
+
+
+use App\Services\CurrentUserResolver;
+use App\Services\ProgressRegistrySystemServiceImpl;
+use App\Student;
+use Tests\TestCase;
+
+class FolderControllerTest extends TestCase
+{
+    public function testIndex() : void{
+
+    }
+
+}
